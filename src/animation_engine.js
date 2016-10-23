c19dffcb26651328e0a4c44e44b85f250814fbce
Nirali Modi <niralimodi@Navdeeps-MacBook-Pro.local>
2016-10-23 15:58:50 +0530

    1. Draw Image. 2. Code clean up

--- src/animation_engine.js
+++ src/animation_engine.js
@@ -1,15 +1,23 @@
 $(document).ready(function(){
-  // var capturer = new CCapture( { format: 'jpg'});
-  var capturer = new CCapture( { format: 'webm'});
+  var format = null;
+  var capturer = null;
 
-  function interpret(input) {
-    var lines = input.split("$$");
-    jQuery.each(lines, function(key, value) {
-      createElements(key, value);
-    });
-    
+  var Group = function Group(text, imagePath){
+    this.text = text;
+    this.imagePath = imagePath;
   }
 
+  function interpret(input) {
+    var groups = input.split("<group>");
+    // currently working on only 1 group
+
+    var group = groups[1];
+    var groupData = group.split("<text>")[1].split("<image>");
+    var textAndImage = new Group(groupData[0], groupData[1]);
+    return textAndImage;
+  }
+  
+//////////////////////////// Using jquery Animation/////////////////////////////////////////////
   function createElements(key, line){
     
       var $div_child = $("<div>", {id: "div_"+key, class: "line"});
@@ -24,65 +32,101 @@ $(document).ready(function(){
             && animate(collection)
         });
   }
-  function animateOnCanvas(collections){
-    console.log(collections);
-    var line = collections.eq(1).text();
+
+//////////////////////////// Drawing on canvas/////////////////////////////////////////////
+
+
+  function animateOnCanvas(textAndImageData){
+    console.log(textAndImageData);
     var canvas = document.querySelector('#myCanvas');
     canvas.style.backgroundColor = 'white';
     
     capturer.start();
-    
-    draw(line, canvas, capturer);
- 
-  }
- 
 
-  function draw(txt, canvas, capturer){
-    
-    var ctx = canvas.getContext("2d"), speed = 4,
-     dashLen = 200, dashOffset = 200, 
-     x = 30, i = 0;
-
-    ctx.font = "20px Monaco, Consolas, Lucida Console, monospace"; 
-    ctx.lineWidth = 1 ; ctx.lineJoin = "round"; ctx.globalAlpha = 1.0;
-    ctx.strokeStyle = ctx.fillStyle = "blue";
-    ctx.clearRect(0,0, 640, 400);
+    var ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, 640, 400);
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    (function loop() {
-     // ctx.clearRect(x, 0, 60, 150);
-     // ctx.fillStyle = "white";
-     // ctx.fillRect(x, 0, 60, 150);
-      ctx.fillStyle = "blue";
-      ctx.setLineDash([dashLen - dashOffset, dashOffset - speed]); // create a long dash mask
-      dashOffset -= speed;                                         // reduce dash length
-      ctx.strokeText(txt[i], x, 50);                               // stroke letter
+    var textStartX = 10,
+        textStartY = 150;
 
-      if (dashOffset > 0) {
-         requestAnimationFrame(loop);             // animate
-        
-      }
-      else {
-        ctx.fillText(txt[i], x, 50);                               // fill final letter
-        dashOffset = dashLen;                                      // prep next char
-        x += ctx.measureText(txt[i++]).width + ctx.lineWidth * Math.random();
-       
-        if (i < txt.length) {
-          requestAnimationFrame(loop);
+    var imageStartX = 10,
+        imageStartY = 20,
+        imageWidth = 100,
+        imageHeight = 100;
 
+    drawText(textAndImageData.text, canvas, textStartX, textStartY);
+    drawImage(textAndImageData.imagePath, canvas, imageStartX, imageStartY, imageWidth, imageHeight);
+
+  }
+  // drawImage
+  function drawImage(imgPath, canvas, xPos, yPos,
+                     imgWidth, imgHeight) {
+    var ctx = canvas.getContext("2d");
+
+    var img = new Image();
+    img.src = imgPath;
+
+    //schedule animation for the next available frame
+    requestAnimationFrame(
+        //wrap animate in a anonymous function to 
+        //match the callback signature 
+        function(timestamp){
+          ctx.drawImage(img, xPos, yPos, imgWidth, imgHeight);
+          
         }
-      }
-      capturer.capture( canvas );
+    );
+    capturer.capture(canvas);
+  
+}
+  
+// draw text
+function drawText(txt, canvas, x, y){
+  
+  var ctx = canvas.getContext("2d");
 
-    })();
-    
+  ctx.font = "20px Monaco, Consolas, Lucida Console, monospace"; 
+  ctx.lineWidth = 1 ; ctx.lineJoin = "round"; ctx.globalAlpha = 1.0;
+  ctx.strokeStyle  = "blue";
+ 
+
+  var speed = 4,
+      dashLen = 200, dashOffset = 200, 
+      i = 0;
+
+  (function loop() {
+  
+    ctx.fillStyle = "blue";
+    ctx.setLineDash([dashLen - dashOffset, dashOffset - speed]); // create a long dash mask
+    dashOffset -= speed;                                         // reduce dash length
+    ctx.strokeText(txt[i], x, y);                              // stroke letter
+
+    if (dashOffset > 0) {
+       requestAnimationFrame(loop);                               // animate
+      
+    }
+    else {
+      ctx.fillText(txt[i], x, y);                               // fill final letter
+      dashOffset = dashLen;                                      // prep next char
+      x += ctx.measureText(txt[i++]).width + ctx.lineWidth ;    // update x for next character
+     
+      if (i < txt.length) {
+        requestAnimationFrame(loop);
+
+      }
+    }
+    capturer.capture(canvas);
+
+  })();
+  
 }  
-   
+  
+////////////////////////////////////////HTML Elements Handling ////////////////////////////////////////
+
 $("#animate").click(function(){
     $('#element').empty();
-    interpret($('#text').val());
-    animateOnCanvas($('.line'));
+    animateOnCanvas(interpret($('#input').val()));
    });
 
 $("#save").click(function(){
@@ -90,6 +134,11 @@ $("#save").click(function(){
    capturer.save();
    });
 
-    
+$('input:radio[name="format"]').change(function(){
 
+    console.log($(this).val());
+    format = $(this).val();
+    capturer = new CCapture( { format: format});
+
+  });
 });
