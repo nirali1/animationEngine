fce37a248db52d8f1200b3a01e8ad6b5bf2845c6
Nirali Modi <niralimodi@Navdeeps-MacBook-Pro.local>
2016-10-23 16:08:42 +0530

    1. Draw Text and Image on canvas. 2. Capture using CCapture plugin. 3. Support jpg and webm formats

--- src/animation_engine.js
+++ src/animation_engine.js
@@ -1,18 +1,29 @@
 $(document).ready(function(){
+  var format = null;
+  var capturer = null;
 
-// function Animation(name, duration){
-//   this.name = name;
-//   this.duration = duration;
-//   this.getInfo = function(){
-//     return this.name + ' ' + this.duration;
-//   };
-// }
+  var Group = function Group(text, imagePath){
+    this.text = text;
+    this.imagePath = imagePath;
+  }
+
   function interpret(input) {
-    var lines = input.split("$$");
-    jQuery.each(lines, function(key, value) {
-      createElements(key, value);
-    });
+    var groups = input.split("<group>");
+    // currently working on only 1 group
+
+    var group = groups[1];
+    var groupData = group.split("<text>")[1].split("<image>");
+    var textAndImage = new Group(groupData[0], groupData[1]);
+    return textAndImage;
+  }
+  
+//////////////////////////// Using jquery Animation/////////////////////////////////////////////
+  function createElements(key, line){
     
+      var $div_child = $("<div>", {id: "div_"+key, class: "line"});
+      $("#element").append($div_child);
+
+      $div_child.text(line).hide();
   }
 
   function animate(collection){
@@ -22,21 +33,112 @@ $(document).ready(function(){
         });
   }
 
-  function createElements(key, line){
-    
-      var $div_child = $("<div>", {id: "div_"+key, class: "line"});
-      $("#element").append($div_child);
+//////////////////////////// Drawing on canvas/////////////////////////////////////////////
 
-      $div_child.text(line).hide();
+
+  function animateOnCanvas(textAndImageData){
+    console.log(textAndImageData);
+    var canvas = document.querySelector('#myCanvas');
+    canvas.style.backgroundColor = 'white';
+    
+    capturer.start();
+
+    var ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    var textStartX = 10,
+        textStartY = 150;
+
+    var imageStartX = 10,
+        imageStartY = 20,
+        imageWidth = 100,
+        imageHeight = 100;
+
+    drawText(textAndImageData.text, canvas, textStartX, textStartY);
+    drawImage(textAndImageData.imagePath, canvas, imageStartX, imageStartY, imageWidth, imageHeight);
+
   }
+  // drawImage
+  function drawImage(imgPath, canvas, xPos, yPos,
+                     imgWidth, imgHeight) {
+    var ctx = canvas.getContext("2d");
+
+    var img = new Image();
+    img.src = imgPath;
+
+    //schedule animation for the next available frame
+    requestAnimationFrame(
+        //wrap animate in a anonymous function to 
+        //match the callback signature 
+        function(timestamp){
+          ctx.drawImage(img, xPos, yPos, imgWidth, imgHeight);
+          
+        }
+    );
+    capturer.capture(canvas);
   
-   
+}
+  
+// draw text
+function drawText(txt, canvas, x, y){
+  
+  var ctx = canvas.getContext("2d");
+
+  ctx.font = "20px Monaco, Consolas, Lucida Console, monospace"; 
+  ctx.lineWidth = 1 ; ctx.lineJoin = "round"; ctx.globalAlpha = 1.0;
+  ctx.strokeStyle  = "blue";
+ 
+
+  var speed = 4,
+      dashLen = 200, dashOffset = 200, 
+      i = 0;
+
+  (function loop() {
+  
+    ctx.fillStyle = "blue";
+    ctx.setLineDash([dashLen - dashOffset, dashOffset - speed]); // create a long dash mask
+    dashOffset -= speed;                                         // reduce dash length
+    ctx.strokeText(txt[i], x, y);                              // stroke letter
+
+    if (dashOffset > 0) {
+       requestAnimationFrame(loop);                               // animate
+      
+    }
+    else {
+      ctx.fillText(txt[i], x, y);                               // fill final letter
+      dashOffset = dashLen;                                      // prep next char
+      x += ctx.measureText(txt[i++]).width + ctx.lineWidth ;    // update x for next character
+     
+      if (i < txt.length) {
+        requestAnimationFrame(loop);
+
+      }
+    }
+    capturer.capture(canvas);
+
+  })();
+  
+}  
+  
+////////////////////////////////////////HTML Elements Handling ////////////////////////////////////////
+
 $("#animate").click(function(){
     $('#element').empty();
-    interpret($('#text').val());
-    animate($('.line'));
+    animateOnCanvas(interpret($('#input').val()));
    });
 
-    
+$("#save").click(function(){
+   capturer.stop();
+   capturer.save();
+   });
 
+$('input:radio[name="format"]').change(function(){
+
+    console.log($(this).val());
+    format = $(this).val();
+    capturer = new CCapture( { format: format});
+
+  });
 });
